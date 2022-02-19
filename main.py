981463169edc392ae0022613420f86f40cea6b6b
EthanGriff88 <ethangriff88@gmail.com>
2022-02-19 15:27:27 +1000

    Improve on_ready message

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print("Now I'm all spooled up!")
     print('------')
     # await client.change_presence(activity = discord.Game('Fortnite'))
     await client.change_presence(activity = discord.Activity(name='Spanked Nuggets', type = discord.ActivityType.watching))
